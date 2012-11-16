7bef68d5289971ff8363d40f7dd188f2de1cfff8
Jakob van Santen <jvansanten@icecube.wisc.edu>
2012-11-16 03:48:36 +0000

    Replace MMC with PROPOSAL. WORD.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/MuonGun@95575 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/shower_and_propagate.py
+++ resources/scripts/shower_and_propagate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from optparse import OptionParser
+from os.path import expandvars
 parser = OptionParser(usage="%prog [OPTIONS] infiles outfile")
 parser.add_option("--run", dest="run", type=int, default=1)
 parser.add_option("--nevents", dest="nevents", type=int, default=int(1e6), help="Number of showers per Corsika file")
@@ -12,6 +13,8 @@ parser.add_option("--primary", dest="primary", default='H', choices=('H', 'He', 
 
 parser.add_option("--atmosphere", dest="atmosphere", type=int, default=11)
 parser.add_option("--model", dest="model", default="SIBYLL", help="Hadronic interaction model", choices=('QGSJET', 'EPOS', 'SIBYLL', 'QGSII', 'DPMJET'))
+
+parser.add_option("--corsika-dir", dest="rundir", type=str, default=expandvars('$I3_BUILD/bin/corsika-run'), help='Coriska run directory')
 
 opts, args = parser.parse_args()
 if len(args) < 1:
@@ -26,22 +29,12 @@ print repr(opts)
 
 from icecube import icetray, dataclasses, dataio
 from icecube.icetray import I3Units
-from icecube import phys_services, c2j_icetray, mmc_icetray
+from icecube import phys_services, sim_services
 from icecube import MuonGun
 icetray.load('corsika-reader')
 from I3Tray import I3Tray
 
-from os.path import expandvars
-
-def MMCFactory(length=10*I3Units.m, seed=12345, mediadef=expandvars('$I3_BUILD/MuonGun/resources/iceworld-mediadef')):
-	jvmOpts = icetray.vector_string()    # fill this with parameters passed directly to the JavaVM
-	jvmOpts.append(expandvars("-Djava.class.path=$I3_BUILD/lib/mmc.jar"))
-	jvmOpts.append("-Xms256m")
-	jvmOpts.append("-Xmx512m")
-	jvmOpts.append("-XX:-HeapDumpOnOutOfMemoryError")
-
-	jvm = c2j_icetray.I3JavaVM(jvmOpts)
-
+def MMCFactory(length=10*I3Units.m, seed=12345, impl='mmc', mediadef=expandvars('$I3_BUILD/MuonGun/resources/iceworld-mediadef')):
 	# Now create the MMC propagators, but first *all* of the options must be set here. 
 	# There's no special options added behind the scenes.  This is much more flexible. 
 	#  Below are the standard options.  To interpret them see the MMC docs.
@@ -51,8 +44,21 @@ def MMCFactory(length=10*I3Units.m, seed=12345, mediadef=expandvars('$I3_BUILD/M
 	mmcOpts += "-radius=100000 "
 	mmcOpts += "-length=%d " % length
 	mmcOpts += "-seed=%d " % seed
+	
+	if impl == 'mmc':
+		from icecube import c2j_icetray, mmc_icetray
+		jvmOpts = icetray.vector_string()    # fill this with parameters passed directly to the JavaVM
+		jvmOpts.append(expandvars("-Djava.class.path=$I3_BUILD/lib/mmc.jar"))
+		jvmOpts.append("-Xms256m")
+		jvmOpts.append("-Xmx512m")
+		jvmOpts.append("-XX:-HeapDumpOnOutOfMemoryError")
+		jvmOpts.append("-XX:+TraceHPI")
 
-	return mmc_icetray.I3PropagatorServiceMMC(jvm,mmcOpts)
+		jvm = c2j_icetray.I3JavaVM(jvmOpts)
+		return mmc_icetray.I3PropagatorServiceMMC(jvm,mmcOpts)
+	else:
+		from icecube import PROPOSAL_icetray
+		return PROPOSAL_icetray.I3PropagatorServicePROPOSAL(mmcOpts)
 
 CORSIKA_CONFIG =\
 """
@@ -69,7 +75,8 @@ OBSLEV  283400.                       observation level (in cm)
 FIXCHI  0.                            starting altitude (g/cm**2)
 MAGNET  16.59 -52.79                  magnetic field centr. Europe
 HADFLG  0  1  0  1  0  2              flags hadr.interact.&fragmentation
-ECUTS   100.  100.  10000000.  100000000.          energy cuts for particles
+ECUTS   300.  300.  10000000.  100000000.          energy cuts for particles
+LOCUT   T   1.58                      zenith-dependent energy threshold
 MUADDI  T                             additional info for muons
 MUMULT  T                             muon multiple scattering angle
 ELMFLG  F   T                         em. interaction flags (NKG,EGS)
@@ -105,16 +112,17 @@ class CorsikaRunner(object):
 		config['seed2'] = random.randint(0, 1<<32 - 1)
 		self.fname = config['outdir'] + 'DAT%.6d' % config['run']
 		os.mkfifo(self.fname)
-		self.p = subprocess.Popen([self.executable % model], stdin=PIPE, stdout=open('/dev/null', 'w'), cwd=rundir)		
+		self.p = Popen([self.executable % model], stdin=PIPE, stdout=open('/dev/null', 'w'), cwd=rundir)		
 		self.p.stdin.write(CORSIKA_CONFIG % config)
 		
 	def __del__(self):
 		if self.fname:
 			os.unlink(self.fname)
 
+import os
 corsika = CorsikaRunner(run=opts.run, nshower=opts.nevents, primary=opts.primary,
     eslope=opts.eslope, emin=opts.emin, emax=opts.emax, atmod=opts.atmosphere,
-    model=opts.model, outdir=os.getcwd()+'/')
+    model=opts.model, outdir=os.getcwd()+'/', rundir=opts.rundir)
 
 tray = I3Tray()
 
@@ -129,7 +137,7 @@ tray.AddModule('I3CORSIKAReader', 'reader', filenamelist=[corsika.fname],
 
 tray.AddModule('Muonitron', 'propatron',
     Depths=[1e3, 1.5e3, 2.e3, 2.5e3, 3.e3, 3.5e3, 4.e3, 4.5e3],
-    MMC=MMCFactory(), CylinderHeight=10.*I3Units.m,
+    MMC=MMCFactory(impl='PROPOSAL'), CylinderHeight=10.*I3Units.m,
 )
 def droppy(frame):
 	# Drop any frames where *no* muons made it to depth
