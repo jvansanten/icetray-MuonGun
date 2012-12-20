3482b5583a0e3e81ae05b0edacf94619844e50a3
Jakob van Santen <jvansanten@icecube.wisc.edu>
2012-12-20 10:44:19 +0000

    Missed commit
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/MuonGun@96952 16731396-06f5-0310-8873-f7f720988828

---
+++ private/pybindings/CORSIKAGenerationProbability.cxx
@@ -0,0 +1,18 @@
+
+#include <MuonGun/CORSIKAGenerationProbability.h>
+#include <MuonGun/Flux.h>
+#include <MuonGun/RadialDistribution.h>
+#include <MuonGun/EnergyDistribution.h>
+
+void register_CORSIKAGenerationProbability()
+{
+	using namespace I3MuonGun;
+	using namespace boost::python;
+	
+	class_<CORSIKAGenerationProbability, boost::shared_ptr<CORSIKAGenerationProbability>, bases<GenerationProbability> >
+	    ("CORSIKAGenerationProbability", init<SamplingSurfacePtr, FluxPtr, RadialDistributionPtr, EnergyDistributionPtr>())
+	    #define RO_PROPS (Surface)(Flux)(RadialDistribution)(EnergyDistribution)
+	    BOOST_PP_SEQ_FOR_EACH(WRAP_PROP_RO, CORSIKAGenerationProbability, RO_PROPS)
+	    #undef RO_PROPS
+	;
+}
