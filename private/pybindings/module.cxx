e554b3c2617cfc632736343ca9b8891cf8903e5a
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-01-31 23:58:44 +0000

    Patch bugs in conditional build.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/MuonGun@98241 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -12,11 +12,11 @@
 namespace bp = boost::python;
 
 #define REGISTER_THESE_THINGS                                           \
-  (I3MuonGun)(CompactTrack)(RadialDistribution)(EnergyDistribution)     \
+  (I3MuonGun)(RadialDistribution)(EnergyDistribution)                   \
   (Flux)(Generator)(Surface)(WeightCalculator)(CanCan)                  \
   (CORSIKAGenerationProbability)
 
-#define REGISTER_EXTRA (histogram)(TrackBinner)(MuonPropagator)
+#define REGISTER_EXTRA (histogram)(TrackBinner)(MuonPropagator)(CompactTrack)
 
 #define I3_REGISTRATION_FN_DECL(r, data, t) void BOOST_PP_CAT(register_,t)();
 #define I3_REGISTER(r, data, t) BOOST_PP_CAT(register_,t)();
