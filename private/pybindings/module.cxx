32cade92cc4b469718dc922a537ae633892d0513
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-02-06 00:49:16 +0000

    Import a copy of boost::numpy, and use it to expose a version of WeightCalculator::GetWeight() that works on the output of MuonBundleConverter.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/MuonGun@98509 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -9,6 +9,10 @@
 #include <icetray/I3FrameObject.h>
 #include <icetray/load_project.h>
 
+#ifdef USE_NUMPY
+#include <boost/numpy.hpp>
+#endif
+
 namespace bp = boost::python;
 
 #define REGISTER_THESE_THINGS                                           \
@@ -27,6 +31,9 @@ BOOST_PP_SEQ_FOR_EACH(I3_REGISTRATION_FN_DECL, ~, REGISTER_EXTRA);
 
 I3_PYTHON_MODULE(MuonGun)
 {
+#ifdef USE_NUMPY
+	boost::numpy::initialize();
+#endif
 	load_project("libMuonGun", false);
 	BOOST_PP_SEQ_FOR_EACH(I3_REGISTER, ~, REGISTER_THESE_THINGS);
 	#ifdef USE_PROPOSAL
