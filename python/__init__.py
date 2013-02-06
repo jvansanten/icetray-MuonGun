14f33f2a0d3f3445c55ebd0a145b80b64ff9d508
Jakob van Santen <jvansanten@icecube.wisc.edu>
2013-02-06 00:44:09 +0000

    Allow base paths to be set in corsika_genprob(), and add a one-stop load_model().
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/MuonGun@98505 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -3,13 +3,18 @@ import icecube.icetray, icecube.dataclasses # be nice and pull in our dependenci
 
 load_pybindings(__name__,__path__)
 
-def corsika_genprob(atmosphere=12, hadronic_model='sibyll'):
-	from os.path import expandvars, join
-	basedir = expandvars('$I3_BUILD/MuonGun/resources/scripts/fitting')
-	config = 'CascadeOptimized'
+def load_model(base):
+	return BundleModel(SplineFlux(base+'.single_flux.fits', base+'.bundle_flux.fits'),
+	    SplineRadialDistribution(base+'.radius.fits'),
+	    SplineEnergyDistribution(base+'.single_energy.fits', base+'.bundle_energy.fits'))
+
+from os.path import expandvars
+def corsika_genprob(config='CascadeOptimized5Comp', atmosphere=12, hadronic_model='sibyll',
+    basedir=expandvars('$I3_BUILD/MuonGun/resources/scripts/fitting')):
+	from os.path import join
 	base = join(basedir, '%s_atmod%d_%s' % (config, atmosphere, hadronic_model.upper()))
 	return CORSIKAGenerationProbability(Cylinder(1600, 800),
 	    SplineFlux(base+'.single_flux.fits', base+'.bundle_flux.fits'),
 	    SplineRadialDistribution(base+'.radius.fits'),
 	    SplineEnergyDistribution(base+'.single_energy.fits', base+'.bundle_energy.fits'))
-
+del expandvars
